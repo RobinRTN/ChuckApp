a3a7af5b8752d0863ad739a354f55597c0fc3380
robin rettien <robinrettien@gmail.com>
2023-03-07 16:55:03 +0100

    added link to user landing page + back link

--- app/javascript/bottom-navbar.js
+++ app/javascript/bottom-navbar.js
@@ -1,7 +1,7 @@
 const navbarItems = document.querySelectorAll('.nav-item-bottom');
 
 navbarItems.forEach((item) => {
-  item.addEventListener('click', (event) => {
+  item.addEventListener('submit', (event) => {
     // Remove the active class from all items
     navbarItems.forEach((item) => {
       item.classList.remove('active');
