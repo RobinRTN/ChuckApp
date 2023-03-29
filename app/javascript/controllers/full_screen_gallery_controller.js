50ac1db82aebb9ca01eef1757f3d4095e16711b9
robin rettien <robinrettien@gmail.com>
2023-03-29 18:02:28 +0200

    added the after previous on landing

---
+++ app/javascript/controllers/full_screen_gallery_controller.js
@@ -0,0 +1,45 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="full-screen-gallery"
+// full_screen_gallery_controller.js
+export default class extends Controller {
+  static targets = ["image", "galleryImage"];
+
+  connect() {
+    this.currentIndex = 0;
+    this.updateImage();
+  }
+
+  showImage(event) {
+    console.log("showing")
+    event.preventDefault();
+    this.currentIndex = parseInt(event.currentTarget.dataset.index);
+    console.log(this.currentIndex)
+    this.updateImage();
+    this.element.classList.add("full-screen-gallery-active");
+  }
+
+  hideGallery() {
+    console.log("hide triggered")
+    this.element.classList.remove("full-screen-gallery-active");
+  }
+
+  prevImage() {
+    console.log("previous triggered")
+    this.currentIndex = (this.currentIndex - 1 + this.galleryImageTargets.length) % this.galleryImageTargets.length;
+    this.updateImage();
+  }
+
+  nextImage() {
+    console.log("next triggered")
+    this.currentIndex = (this.currentIndex + 1) % this.galleryImageTargets.length;
+    this.updateImage();
+  }
+
+  updateImage() {
+    const imageUrl = this.galleryImageTargets[this.currentIndex].dataset.src;
+    console.log(imageUrl)
+    console.log(this.imageTarget)
+    this.imageTarget.src = imageUrl;
+  }
+}
