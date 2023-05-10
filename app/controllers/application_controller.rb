d3fa39a5b5e0123847c84f188dffedfc48f18c91
robin rettien <robinrettien@gmail.com>
2023-05-10 12:17:03 +0200

    added meta elements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_pending_booking
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   private
 
   def set_pending_booking
