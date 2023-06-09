b5de4fb332d4698b5279f1c1abcfeb87aaf9f715
robin rettien <robinrettien@gmail.com>
2023-06-09 23:18:37 +0200

    destryed model Packages

--- app/models/package.rb
+++
@@ -1,9 +0,0 @@
-class Package < ApplicationRecord
-  belongs_to :user
-  has_many :formules
-  accepts_nested_attributes_for :formules, allow_destroy: true
-
-  validates :name, presence: true
-
-
-end
