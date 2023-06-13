8304370ef8886230af078b852fd799f56d7b2c76
robin rettien <robinrettien@gmail.com>
2023-06-13 22:57:25 +0200

    removed for now

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -30,7 +30,7 @@ Devise.setup do |config|
       access_type: "offline",
       prompt: "consent",
       select_account: true,
-      scope: 'userinfo.email, calendar'
+      scope: 'userinfo.email'
     }
 
 
