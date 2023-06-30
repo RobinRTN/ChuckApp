232d4f9c034a3a60230eb6c4eb0ec4e3d21dc661
robin rettien <robinrettien@gmail.com>
2023-06-30 11:34:20 +0200

    added styling and the demo options

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,9 @@ Rails.application.routes.draw do
   get '/finish_reservation/:token', to: 'bookings#finish_reservation', as: 'finish_reservation'
   patch '/update_availability/:id', to: 'bookings#update_availability', as: 'update_availability'
 
+  post '/users/finish_onboarding', to: 'users#finish_onboarding'
+
+
   get 'new_choose_reservation', to: 'bookings#new_choose_reservation', as: 'new_choose_reservation'
   get 'new_finish_reservation', to: 'bookings#new_finish_reservation', as: 'new_finish_reservation'
 
